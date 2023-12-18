af95f6488e05ead893639ba6680442e0c2ef95ac
anandmatkar <139226044+anandmatkar@users.noreply.github.com>
2023-12-18 19:13:24 +0530

    changes in database

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,9 @@
 const { Pool } = require("pg");
 
 let connection = new Pool({
-  user: process.env.USER,
-  host: process.env.HOST,
-  database: process.env.DBNAME,
+  user: "postgres",
+  host: "localhost",
+  database: "rental_space",
   password: "root", //enter your postgres password here
   port: 5432,
 });
