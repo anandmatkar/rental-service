28d414f33111aeb8b58d5fd7780ab79f4204fc7f
anandmatkar <139226044+anandmatkar@users.noreply.github.com>
2023-12-21 18:53:13 +0530

    changes in welcome template

--- src/templates/welcome.js
+++ src/templates/welcome.js
@@ -1,91 +1,110 @@
 module.exports.welcome = function (otp, userName, authLink) {
-  console.log(authLink);
+  console.log(process.env.COMPANY_LOGO);
   let welcomeTemp = `<!DOCTYPE html>
-    <html lang="en">
-      <head>
-        <meta charset="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>OTP Verification</title>
-        <style>
+  <html lang="en">
+  <head>
+      <meta charset="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <title>OTP Verification</title>
+      <style>
           body {
-            font-family: Arial, sans-serif;
-            margin: 0;
-            padding: 0;
-            background-color: #f5f5f5;
+              font-family: "Arial", sans-serif;
+              margin: 0;
+              padding: 0;
+              background: url("https://example.com/background-image.jpg") center/cover no-repeat;
+              display: flex;
+              align-items: center;
+              justify-content: center;
+              height: 100vh;
           }
-    
+  
           .container {
-            max-width: 600px;
-            margin: 0 auto;
-            padding: 30px;
-            background-color: #ffffff;
-            box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.1);
+              max-width: 600px;
+              padding: 30px;
+              background-color: rgba(255, 255, 255, 0.9);
+              box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.1);
+              text-align: center;
+              border-radius: 10px;
+              margin: 20px;
           }
-    
+  
           .container img {
-            max-width: 150px;
-            height: auto;
+              max-width: 150px;
+              height: auto;
+              margin-bottom: 20px;
           }
-    
+  
           .container h1 {
-            font-size: 24px;
-            color: #333333;
-            margin-bottom: 10px;
+              font-size: 24px;
+              color: #333333;
+              margin-bottom: 10px;
           }
-    
+  
           .container p {
-            font-size: 16px;
-            color: #666666;
-            margin-bottom: 20px;
+              font-size: 16px;
+              color: #666666;
+              margin-bottom: 20px;
           }
-    
-          .container .otp-container {
-            display: inline-block;
-            border: 2px solid #dddddd;
-            padding: 20px;
-            margin-bottom: 20px;
-            background-color: #f9f9f9;
-            color: #666666;
-            font-size: 24px;
-            font-weight: bold;
+  
+          .otp-container {
+              display: inline-block;
+              border: 2px solid #dddddd;
+              padding: 20px;
+              margin-bottom: 20px;
+              background-color: #f9f9f9;
+              color: #333333;
+              font-size: 24px;
+              font-weight: bold;
           }
-    
+  
           .container .otp-container span {
-            font-size: 16px;
-            color: #666666;
-            margin-top: 10px;
-            display: block;
+              font-size: 16px;
+              color: #666666;
+              margin-top: 10px;
+              display: block;
           }
-    
-          .container .button {
-            display: inline-block;
-            padding: 10px 20px;
-            background-color: #333333;
-            color: #ffffff;
-            text-decoration: none;
-            border-radius: 5px;
+  
+          .button {
+              display: inline-block;
+              padding: 10px 20px;
+              background-color: #1bb2cc;
+              color: #ffffff;
+              text-decoration: none;
+              border-radius: 5px;
+              transition: background-color 0.3s ease;
           }
-        </style>
-      </head>
-      <body>
-        <div class="container">
-          <img src="{process.env.COMPANY_LOGO}" alt="Logo" />
+  
+          .button:hover {
+              background-color: #126d89;
+          }
+      </style>
+  </head>
+  
+  <body>
+      <div class="container">
+          <img src="${process.env.COMPANY_LOGO}" alt="Logo" />
           <h1>OTP Verification</h1>
           <p>Dear ${userName},</p>
-          <p>Your one-time password (OTP) for account verification is:</p>
-          <div class="otp-container">
-            ${otp}
-          </div>
-          Please click on the given link for <a href="${authLink}" style="color:#1bb2cc;"><u>verify</u></a><br/>
+           Welcome to Rental.com.
+        <p>Your one-time password (OTP) for account verification is:</p>
+          <div class="otp-container">${otp}</div>
           <p>
-            Welcome to Rental.com. If you have any further queries, feel free to
-            reach out to us at support@rental.com
+              Please click the button below to
+              <a href="${authLink}" class="button"><u>Verify</u></a>
+             
+          </p>
+          <p>
+            If you have any further queries, feel free to
+              reach out to us at
+              <a href="mailto:support@rental.com">support@rental.com</a>
           </p>
           <p>Thank you for using our service!</p>
-        </div>
-      </body>
-    </html>
-    `
+        
+      </div>
+  </body>
+  
+  </html>
+  `
 
   return welcomeTemp
 }
