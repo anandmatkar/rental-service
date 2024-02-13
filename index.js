09617507984cad3d490f591e9e6f7d24e3340168
anandmatkar <anandmatkar123#>
2024-02-13 17:09:40 +0530

    changes in index.js file

--- index.js
+++ index.js
@@ -46,56 +46,6 @@ if (cluster.isMaster) {
     console.log(`Worker ${process.pid} started on ${process.env.PORT}`)
   );
 
-  // const io = require('socket.io')(server, {
-  //   cors: {
-  //     origin: "*",
-  //     credentials: true,
-  //   },
-  // });
-
-  // const onlineUsers = new Map();
-
-  // io.on("connection", (socket) => {
-  //   console.log("user connected");
-  //   global.chatSocket = socket;
-  //   let userId;
-
-  //   socket.on("add-user", (user) => {
-  //     userId = user;
-  //     onlineUsers.set(userId, socket.id);
-
-  //     // Fetch notifications for the user and emit to their socket connection
-  //     fetchInstantForUser(userId)
-  //       .then((notifications) => {
-  //         console.log(notifications, "1111111111111");
-  //         emitNotificationsToUser(socket, notifications); // Emit notifications to the user
-  //       })
-  //       .catch((error) => {
-  //         console.error("Error fetching notifications:", error);
-  //       });
-  //   });
-
-  //   socket.on("disconnect", (userData) => {
-  //     onlineUsers.delete(userId);
-  //     console.log('User disconnected');
-  //   });
-
-  //   socket.on("send-msg", (data) => {
-  //     console.log(data, "send-msg");
-  //     const sendUserSocket = onlineUsers.get(data.to);
-
-  //     if (sendUserSocket) {
-  //       socket.to(sendUserSocket).emit("msg-recieve", data.message_content);
-  //     }
-  //   });
-  // });
-
-  // // Function to emit notifications to a specific user
-  // function emitNotificationsToUser(socket, notifications) {
-  //   console.log(notifications, "111111111");
-  //   socket.emit("notifications", notifications);
-  // }
-
   const io = require('socket.io')(server, {
     cors: {
       origin: "*",
@@ -105,53 +55,47 @@ if (cluster.isMaster) {
 
   const onlineUsers = new Map();
 
-  io.on("connection", async (socket) => {
+  io.on("connection", (socket) => {
     console.log("user connected");
+    global.chatSocket = socket;
     let userId;
 
     socket.on("add-user", (user) => {
       userId = user;
       onlineUsers.set(userId, socket.id);
+
+      // Fetch notifications for the user and emit to their socket connection
+      fetchInstantForUser(userId)
+        .then((notifications) => {
+          console.log(notifications, "1111111111111");
+          emitNotificationsToUser(socket, notifications); // Emit notifications to the user
+        })
+        .catch((error) => {
+          console.error("Error fetching notifications:", error);
+        });
+    });
+
+    socket.on("disconnect", (userData) => {
+      onlineUsers.delete(userId);
+      console.log('User disconnected');
     });
 
     socket.on("send-msg", (data) => {
+      console.log(data, "send-msg");
       const sendUserSocket = onlineUsers.get(data.to);
 
       if (sendUserSocket) {
         socket.to(sendUserSocket).emit("msg-recieve", data.message_content);
       }
     });
-
-    // Function to fetch and emit notifications for a user
-    const emitNotifications = async (userId) => {
-      try {
-        const notifications = await fetchInstantForUser(userId);
-        console.log(notifications, "emit notifications");
-        const sendUserSocket = onlineUsers.get(userId);
-
-        if (sendUserSocket && notifications.length > 0) {
-          socket.to(sendUserSocket).emit("notifications", notifications);
-        }
-      } catch (error) {
-        console.error("Error fetching and emitting notifications:", error);
-      }
-    };
-
-    // Emit notifications when a user is added
-    socket.on("add-user", (user) => {
-      userId = user;
-      onlineUsers.set(userId, socket.id);
-      emitNotifications(userId); // Emit notifications for the newly added user
-    });
-
-    // Emit notifications when requested by the client
-    socket.on("fetch-notifications", () => {
-      if (userId) {
-        console.log(userId, "useridddddddd");
-        emitNotifications(userId);
-      }
-    });
   });
+
+  // Function to emit notifications to a specific user
+  function emitNotificationsToUser(socket, notifications) {
+    console.log(notifications, "111111111");
+    socket.emit("notifications", notifications);
+  }
+
 
   app.use('/api/v1', Router);
 
@@ -161,3 +105,5 @@ if (cluster.isMaster) {
     });
   });
 }
+
+// module.exports.io = io;
