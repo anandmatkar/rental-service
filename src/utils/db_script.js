33907de309c8a053d28e10463caee903b0e1cf98
anandmatkar <anandmatkar123#>
2024-02-14 18:13:36 +0530

    changes in getMessage API

--- src/utils/db_script.js
+++ src/utils/db_script.js
@@ -318,7 +318,7 @@ const db_sql = {
                 rental_items.total_price,
                 rental_items.image;`,
     Q47: `INSERT INTO messages(message_content,sender_id,receiver_id) VALUES('{var1}', '{var2}', '{var3}') RETURNING *`,
-    Q48: `SELECT * FROM messages WHERE sender_id = '{var1}' AND receiver_id = '{var2}' OR sender_id = '{var2}' AND receiver_id = '{var1}' AND deleted_at IS NULL `,
+    Q48: `SELECT * FROM messages WHERE sender_id = '{var1}' AND receiver_id = '{var2}' OR sender_id = '{var2}' AND receiver_id = '{var1}' AND deleted_at IS NULL ORDER BY created_at ASC `,
     Q49: `DELETE FROM messages WHERE id = '{var1}' AND deleted_at IS NULL RETURNING *`,
     Q50: `INSERT INTO item_images(user_id, items_id, path) VALUES('{var1}','{var2}', '{var3}') RETURNING *`,
     Q51: `INSERT INTO notifications(user_id, message, type_id, type) VALUES('{var1}', '{var2}','{var3}', '{var4}') RETURNING *`,
